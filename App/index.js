011a5c0fa17dc3cc2a38a2eb45583ec076c4b5ca
Jd13z <josue.aquise@gmail.com>
2021-03-17 16:48:28 -0700

    Actualización Información educación

--- App/index.js
+++ App/index.js
@@ -11,7 +11,7 @@ btnscrap.addEventListener('click', async ()=>{
     }
 })
 
-const scrapingProfile = ()=>{
+const scrapingProfile = async ()=>{
     const wait = function(milliseconds){
         return new Promise(function(resolve){
             setTimeout(function() {
@@ -21,29 +21,55 @@ const scrapingProfile = ()=>{
 
     };
 
+    const profileInfo = {
+        personalInfo : {},
+        educationInfo : new Array()
+
+    };
 
     //Personal information
     const elementNameProfile = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 ul li")
     const elementNameTitle = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 h2")
     const elementNameLocation = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 ul.pv-top-card--list-bullet li")
 
-    const name = elementNameProfile? elementNameProfile.innerText:'';
-    const title = elementNameTitle? elementNameTitle.innerText:'';
-    const location = elementNameLocation? elementNameLocation.innerText:'';
-
-    //Education informaction
-    const elementsEducation = document.querySelectorAll('#education-section ul li');
-    elementsEducation.forEach((element => {
-        
-    }));
-
-
+    //Aditional information
     wait(2000)
     const elementMoreResume = document.getElementById('line-clamp-show-more-button')
     if(elementMoreResume) elementMoreResume.click();
     const elementResume = document.querySelector('section.pv-about-section > p')
-    const resume = elementResume.innerText
 
+    profileInfo.personalInfo.name = elementNameProfile? elementNameProfile.innerText:'';
+    profileInfo.personalInfo.title = elementNameTitle? elementNameTitle.innerText:'';
+    profileInfo.personalInfo.location = elementNameLocation? elementNameLocation.innerText:'';
+    profileInfo.personalInfo.resume = elementResume.innerText
 
-    console.log({name,title,resume,location})
+    //Education informaction
+    const elementsEducation = document.querySelectorAll('#education-section ul li');
+    const elementMoreEducation = document.getElementById('ember174');
+    if(elementMoreEducation && elementMoreEducation.children) {
+        console.log(elementMoreEducation, elementMoreEducation.attributes);
+        elementMoreEducation.children[0].click();
+        await sleep(2000);
+    }
+
+    elementsEducation.forEach((element => {
+        const education = {};
+        const elementEducationSchoolName = element.querySelector('.pv-entity__school-name');
+        const elementEducationTitulation = element.querySelector('.pv-entity__secondary-title.pv-entity__degree-name');
+        const elementEducationDiscipline = element.querySelector('.pv-entity__secondary-title.pv-entity__fos');
+        const elementEducationDates = element.querySelector('.pv-entity__dates');
+
+        education.schoolName = elementEducationSchoolName? elementEducationSchoolName.innerText:'';
+        education.titulation = elementEducationTitulation? elementEducationTitulation.lastElementChild.innerText:'';
+        education.discipline = elementEducationDiscipline? elementEducationDiscipline.lastElementChild.innerText:'';
+        education.dates = elementEducationDates? elementEducationDates.lastElementChild.innerText:'';
+
+        profileInfo.educationInfo.push(education);
+    }));
+
+    console.log(profileInfo);
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
 }
